7841de342fcc6b85575751da6ae41e476564243f
SalahAmer2 <salahayman2@gmail.com>
2020-11-29 02:03:13 +0200

    trying to deploy

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const path = require('path');
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 const routes = require('./routes/api');
 
@@ -21,5 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 
 app.use('/api', routes);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === '') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server starting at ${PORT}`));
